2dcf734a165c6789b9b58a7bbae72cbe4f1d04a6
Andrew Burns <anditburns@gmail.com>
2019-08-12 11:44:55 +0800

    logging now to ubuntu

--- Marvin/Marvin-Ubuntu/Program.cs
+++ Marvin/Marvin-Ubuntu/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
 using Serilog;
+using SharedEntities;
 
 namespace Marvin_Ubuntu
 {
     class Program
     {
-
+        private static Marvin.Marvin _marvin;
 
         public static void Main(string[] args)
         {
             try
             {
-                var logPath = "/var/tmp/log.txt";
+                var appSettings = GetAppSettings();
+                var logger = new LoggerConfiguration()
+                                  .MinimumLevel.Information()
+                                  .WriteTo.File(appSettings.LogPath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+                                  .CreateLogger();
 
-                Log.Logger = new LoggerConfiguration()
-                                    .MinimumLevel.Information()
-                                    .WriteTo.File(logPath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
-                                    .CreateLogger();
-
-                Log.Information($"Starting PublishCron");
-               
-
-                Log.Information($"PublishCron finished. Closing & flushing logs...");
+                _marvin = new Marvin.Marvin(appSettings, logger);
+                _marvin.Run();
             }
             catch (Exception ex)
             {
@@ -31,6 +32,22 @@ namespace Marvin_Ubuntu
             {
                 Log.CloseAndFlush();
             }
+        }
+
+        static AppSettings GetAppSettings()
+        {
+            var appSettingsFilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/appsettings.json";
+
+            if (!File.Exists(appSettingsFilePath))
+            {
+                throw new FileNotFoundException("appsettings.json was not found or is not accessible");
+            }
+
+            var appSettingsContent = File.ReadAllText(appSettingsFilePath);
+            if (string.IsNullOrEmpty(appSettingsContent))
+                throw new Exception("Appsettings file is empty");
+
+            return JsonConvert.DeserializeObject<AppSettings>(appSettingsContent);
         }
     }
 }
