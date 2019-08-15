c16c8b8bd37979d3cf36704865252e1aafc31a44
Andrew Burns <anditburns@gmail.com>
2019-08-16 07:47:32 +0800

    adding functionality to create new wallet/keystore

---
+++ Marvin/Wallet/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Wallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            do
+            {
+                userInput = DisplayMenu();
+
+                switch (userInput)
+                {
+                    case 1:
+                        CreateKeyStore();
+                        break;
+                    case 2:
+                   //     DisplayBalance();
+                        break;
+                    
+
+                }
+            } while (userInput != 5);
+
+        }
+
+        static int DisplayMenu()
+        {
+            Console.WriteLine("Choose and option...");
+            Console.WriteLine();
+            Console.WriteLine("1. Create Keystore");
+            Console.WriteLine("2. View Balance");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+
+        static void CreateKeyStore()
+        {
+            Console.WriteLine("Enter location to save Keystore file...");
+            Console.WriteLine("--eg: F:/temp/keystore.icx");
+            var result = string.Empty;
+
+            while(string.IsNullOrWhiteSpace(result))
+            { 
+                Console.WriteLine("File path cannot be blank please try again");
+                Console.WriteLine("Enter location to save Keystore file...");
+                result = Console.ReadLine();
+            }
+
+
+
+        }
+
+    }
+}
