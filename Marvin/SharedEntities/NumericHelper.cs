77d40cf631373fdd91d171f14fb7870f0a06ebdc
Andrew Burns <anditburns@gmail.com>
2019-08-18 00:22:52 +0800

    adding basic wallet functionality. 1. Create new wallet/keystore 2. Check balance

---
+++ Marvin/SharedEntities/NumericHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SharedEntities
+{
+    public static class NumericHelper
+    {
+        public static string Loop2ICX(string bigInt, bool trimTrailing = true)
+        {
+            BigInteger icxloop = BigInteger.Parse(bigInt);
+
+            double result = (double)icxloop;
+
+            double icx = result / Math.Pow(10, 18);
+
+            return icx.ToString();
+        }
+    }
+}
--- Marvin/Wallet/Program.cs
+++ Marvin/Wallet/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Numerics;
+using IconSDK;
+using IconSDK.Account;
+using IconSDK.Types;
+using SharedEntities;
 
 namespace Wallet
 {
@@ -7,51 +13,118 @@ namespace Wallet
         static void Main(string[] args)
         {
             int userInput = 0;
-            do
+            while(userInput != 3)
             {
                 userInput = DisplayMenu();
-
                 switch (userInput)
                 {
                     case 1:
                         CreateKeyStore();
                         break;
                     case 2:
-                   //     DisplayBalance();
+                        DisplayBalance();
                         break;
-                    
-
                 }
-            } while (userInput != 5);
-
+            } 
         }
 
         static int DisplayMenu()
         {
-            Console.WriteLine("Choose and option...");
+            Console.WriteLine("Choose an option...");
             Console.WriteLine();
             Console.WriteLine("1. Create Keystore");
-            Console.WriteLine("2. View Balance");
+            Console.WriteLine("2. Show Balance");
+            Console.WriteLine("3. Exit");
             var result = Console.ReadLine();
             return Convert.ToInt32(result);
         }
 
         static void CreateKeyStore()
         {
-            Console.WriteLine("Enter location to save Keystore file...");
-            Console.WriteLine("--eg: F:/temp/keystore.icx");
-            var result = string.Empty;
+            var keyStorePath = string.Empty;
+            var match = false;
 
-            while(string.IsNullOrWhiteSpace(result))
+            while(string.IsNullOrWhiteSpace(keyStorePath))
             { 
-                Console.WriteLine("File path cannot be blank please try again");
-                Console.WriteLine("Enter location to save Keystore file...");
-                result = Console.ReadLine();
+                Console.WriteLine("Enter location to save Keystore file");
+                Console.WriteLine("--eg: F:/temp/keystore.icx");
+                keyStorePath = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(keyStorePath))
+                {
+                    Console.WriteLine("File path cannot be blank please try again");
+                }
+            }
+
+            var keyStorePassword1 = string.Empty;
+            var keyStorePassword2 = string.Empty;
+        
+            while(!match)
+            {  
+                Console.WriteLine("Enter password for your Keystore...");
+                keyStorePassword1 = Console.ReadLine();
+                Console.WriteLine("Enter password again for verification");
+                keyStorePassword2 = Console.ReadLine();
+
+                if (keyStorePassword1.Equals(keyStorePassword2))
+                {
+                    match = true;
+                }
+                else
+                {
+                   Console.WriteLine("Passwords do not match");
+                }
             }
 
 
+            var wallet = IconSDK.Account.Wallet.Create();
+            Console.WriteLine("Creating Keystore please wait...");
+            var keyStore = wallet.Store(keyStorePassword1, keyStorePath);
 
+            if (!string.IsNullOrWhiteSpace(keyStore))
+            {
+                Console.WriteLine($"Keystore file created successfully at : {keyStorePath}");
+                Console.WriteLine($"Your public address is : {wallet.Address}");
+                Console.WriteLine($"You will need to send ICX to this address to use Marvin");
+            }
         }
+        static void DisplayBalance()
+        {
+            var keyStorePath = string.Empty;
 
+            while (string.IsNullOrWhiteSpace(keyStorePath))
+            {
+                Console.WriteLine("Enter location to save Keystore file");
+                Console.WriteLine("--eg: F:/temp/keystore.icx");
+                keyStorePath = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(keyStorePath))
+                {
+                    Console.WriteLine("File path cannot be blank please try again");
+                }
+            }
+
+            if (File.Exists(keyStorePath))
+            {
+                // This path is a file
+                Console.WriteLine("Enter password for your Keystore...");
+                var keyStorePassword = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine("Checking balance please wait...");
+                    var wallet = IconSDK.Account.Wallet.Load(keyStorePassword, keyStorePath);
+                    var balance  = wallet.GetBalance().Result;
+                    Console.WriteLine($"Your current ICX balance is : {NumericHelper.Loop2ICX(balance.ToString())} ICX");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Unable to open Keystore, did you type the correct password?");
+                    DisplayBalance();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Marvin couldn't find a keystore file at that location, please try again.");
+                DisplayBalance();
+            }
+        }
     }
 }
